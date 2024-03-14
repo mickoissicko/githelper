1d00fbec237c4aaaefdf13a1cd24d66e65b74477
mickoissicko <mickeysidd@proton.me>
2024-03-14 22:10:35 +0100

    fixed formatting for opening with write mode && remove extra tokens

--- src/reset/clean.c
+++ src/reset/clean.c
@@ -4,9 +4,8 @@ void resetcfg()
 {
     FILE *Preferences;
 
-    Preferences = fopen("cfg/pref.yml", 'w');
+    Preferences = fopen("cfg/pref.yml", "w");
 
-    fprintf(Preferences, '\n');
     fprintf(Preferences, "\n");
 
     fclose(Preferences);
