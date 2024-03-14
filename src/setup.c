282538947f9ba6f77f9e2b4ae43d257f9516d48b
mickoissicko <mickeysidd@proton.me>
2024-03-14 16:08:45 +0100

    adds the path

--- src/setup.c
+++ src/setup.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-void setup()
-{
-    // test
+#define MAXLEN 1024
 
+void setup() {
+    FILE *Preferences;
+    char data[MAXLEN];
+
+    Preferences = fopen("cfg/pref", "w");
+
+    printf("Path to .git folder: ");
+    scanf("%s", &data);
+
+    fwrite(data, sizeof(char), sizeof(data), Preferences);
+    fclose(Preferences);
+ 
+    return 0;
 }
