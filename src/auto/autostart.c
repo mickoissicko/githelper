6039f4751a6f3c02d5ab8d4a529e6f12ba2f7e95
mickoissicko <defualtusermick@gmail.com>
2024-03-15 18:44:08 +0100

    add alt configger

--- src/auto/autostart.c
+++ src/auto/autostart.c
@@ -21,7 +21,8 @@ void autostart()
 
     if (Cfg == NULL)
     {
-        exit(1);
+        alt_cfgger();
+        exit(0);
     }
 
     while (fgets(Line, MAX_LN_LEN, Cfg))
@@ -44,4 +45,59 @@ void autostart()
     }
 
     return;
+}
+
+void alt_cfgger()
+{
+    FILE* Dgh; // dot git helper file
+    FILE* Pyf; // path to yaml file
+    FILE* Cfg;
+
+    bool Auto;
+
+    char Line[MAX_LN_LEN];
+    char Auto_On[] = "Autostart: On";
+    char Auto_Off[] = "Autostart: Off";
+    char* Filename;
+
+    Dgh = fopen(".githelper", "r");
+
+    if (Dgh == NULL)
+    {
+        printf("'.githelper' file needed if project does not contain YAML configuration files\n");
+        exit(1);
+    }
+
+    fgets(Line, MAX_LN_LEN, Dgh);
+    Filename = Line;
+    fclose(Dgh);
+
+    Cfg = fopen(Filename, "r");
+    
+    if (Cfg == NULL)
+    {
+        printf("Invalid path provided in '.githelper'!\n");
+        exit(1);
+    }
+
+    while(fgets(Line, MAX_LN_LEN, Cfg))
+    {
+        if (strstr(Line, Auto_On))
+        {
+            Auto = true;
+        }
+
+        else if (strstr(Line, Auto_Off))
+        {
+            Auto = false;
+        }
+    }
+
+    if (Auto)
+    {
+        cmds();
+        exit(0);
+    }
+
+    fclose(Cfg);
 }
