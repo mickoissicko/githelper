89f9e0ae207b0213f7936640e02398b15cb56325
mickoissicko <defualtusermick@gmail.com>
2024-03-18 11:05:15 +0100

    test #3

--- src/main.c
+++ src/main.c
@@ -6,10 +6,6 @@
 
 int main(int argc, char* argv[])
 {
-    char Ui;
-    const int INFINITE = 1;
-    int err = 1;
-
     if (argc == 2)
     {
         if (strcmp(argv[1], "-cfg") == 0)
@@ -18,47 +14,13 @@ int main(int argc, char* argv[])
 
             return 0;
         }
+
         printf("Unrecognised argument\n");
 
         return 0;
     }
 
     start();
-
-    if (err == 1)
-    {
-        while (INFINITE)
-        {
-            printf("Git Helper [v1.1.1]\n");
-            printf("\n");
-            printf("Hello user!\n");
-            printf("You are seeing this screen because you haven't: \n");
-            printf("- Configured '.githelper'\n");
-            printf("- Or have the 'pref.yml' file\n");
-            printf("Read the docs to find out more!\n");
-            printf("\n");
-            printf("Remember: you need to:\n");
-            printf("- Configure an absolute path in .githelper\n");
-            printf("For example: /home/user/file.txt is correct, but\n");
-            printf("~/file.txt isn't");
-            printf("Also, the .githelper file can not end in a newline.\n");
-            printf("It needs to have the path, no extra characters!\n");
-            printf("\n");
-            printf("You can also run GitHelper with '-cfg' to make the configuration file!\n");
-            printf("Type X to exit and acknowledge...\n");
-
-            printf("> ");
-            scanf("%c", &Ui);
-
-            switch (Ui)
-            {
-                case 'X':
-                    return 1;
-                case 'x':
-                    return 1;
-            }
-        }
-    }
 
     return 0;
 }
