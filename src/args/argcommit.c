5a87ca370ea036fdd15c5dba61a23cc13732d97e
mickoissicko <defualtusermick@gmail.com>
2024-03-18 12:07:48 +0100

    add function parametres

--- src/args/argcommit.c
+++ src/args/argcommit.c
@@ -1,7 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-void argcommit(bool custom)
-{
-
+void argcommit(
+    bool all,
+    bool cl,
+    bool mk,
+    bool ap
+){
+    // nothing here yet
 }
