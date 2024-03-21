90d6feffaa89c18ac65c118487dfdd8354995d38
mickoissicko <defualtusermick@gmail.com>
2024-03-21 15:43:32 +0100

    remove push function, moved to a standalone file

--- src/args/argparse.c
+++ src/args/argparse.c
@@ -116,27 +116,3 @@ void StageTwo()
 
     RegularExecution();
 }
-
-void PromptForPush()
-{
-    char Ui;
-
-    do
-    {
-        printf("Push? [y/n]: ");
-        scanf(" %c", &Ui);
-    }
-    while (Ui != 'Y' && Ui != 'y' && Ui != 'N' && Ui != 'n');
-
-    switch (Ui)
-    {
-        case 'Y':
-        case 'y':
-            system("git push");
-            break;
-
-        case 'N':
-        case 'n':
-            break;
-    }
-}
