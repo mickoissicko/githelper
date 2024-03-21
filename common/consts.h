2c79a899f10db57381c5743ce61aeb0d60ff13b1
mickoissicko <defualtusermick@gmail.com>
2024-03-21 12:06:17 +0100

    defined new constants, but commented them out as we do not need them at the moment

--- common/consts.h
+++ common/consts.h
@@ -5,4 +5,12 @@
 const static char Com[] = "commit";
 const static char Add[] = "addfiles";
 
+/*
+const char arg_detached_2[] = "--detached";
+const char arg_keepfile_2[] = "--keepfile";
+const char arg_config_yml[] = "-cfg";
+const char arg_detached_1[] = "--d";
+const char arg_keepfile_1[] = "--k";
+*/
+
 #define MAX_ARG 4
