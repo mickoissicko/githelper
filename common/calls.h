f38e040d13bdcdf3bc5bce8f45ee33e31b6e9f53
mickoissicko <defualtusermick@gmail.com>
2024-03-23 18:49:28 +0100

    remove unused funcs

--- common/calls.h
+++ common/calls.h
@@ -6,10 +6,7 @@
 
 int QuickStart(int argc, char* argv[]);
 int argparse(int argc, char* argv[]);
-void cmds(bool custom);
 void RegularExecution();
 void PromptForPush();
 void StageTwo();
 void commit();
-void setup();
-void start();
