a048e8409e26fa5c7148da221e6e6a936ea01365
mickoissicko <defualtusermick@gmail.com>
2024-03-23 18:42:31 +0100

    move quickstart -> start/quickstart.c

---
+++ src/start/quickstart.c
@@ -0,0 +1,42 @@
+#include "../../common/consts.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int QuickStart(int argc, char* argv[])
+{
+    if (argc < MIN_ARG) return 1;
+
+    unsigned long LEN = MAX_STR_LEN;
+
+    char SystemFormattedMessage[MAX_STR_LEN];
+    char SystemFormattedAdd[MAX_STR_LEN];
+    char CommitMessage[MAX_STR_LEN];
+    char ShowRelation[MAX_STR_LEN];
+    char FilePath[MAX_STR_LEN];
+
+    strcpy(CommitMessage, argv[4]); // '--q' _str
+    strcpy(ShowRelation,  argv[3]); // '--q' _str
+    strcpy(FilePath,      argv[2]); // '--q' _str
+
+    if (strcmp(ShowRelation, "::") == 0)
+    {
+        snprintf(SystemFormattedMessage, LEN, "git commit -m \"%s\"", CommitMessage);
+        snprintf(SystemFormattedAdd, LEN, "git add \"%s\"", FilePath);
+
+        system(SystemFormattedAdd);
+        system(SystemFormattedMessage);
+
+        if (strcmp(argv[5], "!push") == 0)
+        {
+            system("git push");
+
+            return 0;
+        }
+    }
+
+    else return 1;
+
+    return 0;
+}
