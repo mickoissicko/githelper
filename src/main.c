b5a0be111ea055a97a70f890aaed19321f889260
mickoissicko <defualtusermick@gmail.com>
2024-03-23 17:34:59 +0100

    add rule for '--q' && remove unused debug functions

--- src/main.c
+++ src/main.c
@@ -1,28 +1,32 @@
 // main.c
 #include "../common/calls.h"
 
-#include <stdio.h>
 #include <string.h>
 
 int main(int argc, char** argv)
 {
     int index = 1;
 
-    puts("starting...");
-
     bool keepfile = false;
     bool detached = false;
     bool filenull = false;
     bool genprefs = false;
+
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "--q") == 0)
+        {
+            QuickStart(argc, argv);
+
+            return 0;
+        }
+    }
 
     while (index < argc)
     {
         if (!strcmp(argv[index], "--d")) detached = true;
         if (!strcmp(argv[index], "--k")) keepfile = true;
         if (!strcmp(argv[index], "-cfg")) genprefs = true;
-
-        //if (!strcmp(argv[index], "--detached")) detached = true;
-        //if (!strcmp(argv[index], "--keepfile")) keepfile = true;
 
         index++;
     }
