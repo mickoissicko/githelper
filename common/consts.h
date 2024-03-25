31a71b9050dd16822c2c8ffceddcae78295ab231
mickoissicko <defualtusermick@gmail.com>
2024-03-25 12:45:53 +0100

    some comments + new constants defined

--- common/consts.h
+++ common/consts.h
@@ -2,18 +2,15 @@
     #define CONSTS_H
 #endif
 
-const static char Com[] = "commit";
-const static char Add[] = "addfiles";
-
-/*
-const char arg_detached_2[] = "--detached";
-const char arg_keepfile_2[] = "--keepfile";
-const char arg_config_yml[] = "-cfg";
-const char arg_detached_1[] = "--d";
-const char arg_keepfile_1[] = "--k";
-*/
-
+// Macros
 #define MAX_STR_LEN 4096
 #define MAX_LEN 2048
 #define MAX_ARG 6
 #define MIN_ARG 5
+
+// Strings
+const static char Com[] = "commit";
+const static char Add[] = "addfiles";
+const static char Adr[] = "https://github.com/";
+const static char Aur[] = "https://aur.archlinux.org/";
+const static char Tmp[] = "~/aur.gelper_tmp";
