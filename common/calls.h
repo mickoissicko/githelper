c5f13996e1fed7fafc4bd07bf8305eec500a3ce7
mickoissicko <defualtusermick@gmail.com>
2024-03-25 12:45:40 +0100

    use new func calls

--- common/calls.h
+++ common/calls.h
@@ -4,9 +4,14 @@
 
 #include <stdbool.h>
 
+int AdvancedCopy(int argc, char* argv[]);
 int QuickStart(int argc, char* argv[]);
-int argparse(int argc, char* argv[]);
+int ArgParse(int argc, char* argv[]);
+
+
 void RegularExecution();
 void PromptForPush();
+void DeleteTemp();
+void CreateTemp();
 void StageTwo();
-void commit();
+void Commit();
