f9b7ad3c5e9280b0ce0dc09cd9339932c5f65998
mickoissicko <defualtusermick@gmail.com>
2024-03-26 11:50:22 +0100

    add new function

--- common/calls.h
+++ common/calls.h
@@ -1,12 +1,16 @@
 #ifndef CALLS_H
     #define CALLS_H
+#include <stdnoreturn.h>
 #endif
 
 #include <stdbool.h>
 
+void VerifyArguments(int argc, char* argv[]);
+
 int AdvancedCopy(int argc, char* argv[]);
 int DirectCopy(int argc, char* argv[]);
 int QuickStart(int argc, char* argv[]);
+int DirectAUR(int argc, char** argv);
 int ArgParse(int argc, char* argv[]);
 
 void RegularExecution();
