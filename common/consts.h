2df126382706d0834ef6297ec43c12c00f77986a
mickoissicko <defualtusermick@gmail.com>
2024-03-26 10:31:59 +0100

    remove useless consts

--- common/consts.h
+++ common/consts.h
@@ -9,8 +9,5 @@
 #define MIN_ARG 5
 
 // Strings
-const static char Com[] = "commit";
-const static char Add[] = "addfiles";
 const static char Adr[] = "https://github.com/";
 const static char Aur[] = "https://aur.archlinux.org/";
-const static char Tmp[] = "~/aur.gelper_tmp";
